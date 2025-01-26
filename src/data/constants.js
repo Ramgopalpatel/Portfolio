22467f3e582965472d64e1102be068081c6aa066
rgpatel <rgpatel119@gmail.com>
2025-01-26 20:38:33 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,273 @@
+export const Bio = {
+  name: "Ramgopal Patel",
+  roles: [
+    "Full Stack Developer",
+    "Frontend Developer",
+    "Software Developer",
+    "Data Analyst"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Ramgopalpatel",
+  resume: "https://www.linkedin.com/in/ramgopal-patel-1bb669209/",
+  linkedin: "https://www.linkedin.com/in/ramgopal-patel-1bb669209/",
+  twitter: "https://x.com/RAMGOPA7047214",
+  insta: "https://www.instagram.com/rgpatel119/",
+  facebook: "https://www.facebook.com/ramgopal.patel.169067/",
+  email: "mailto:rgpatel119@gmail.com",
+  whatsApp: "https://wa.me/7691912149",
+};
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Tailwind",
+          image:
+            "https://w7.pngwing.com/pngs/771/978/png-transparent-tailwind-css-css-framework-customizable-low-level-tailwind-logo-3d-icon.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:
+            "https://w7.pngwing.com/pngs/545/451/png-transparent-node-js-express-js-javascript-solution-stack-web-application-others-angle-text-rectangle-thumbnail.png",
+        },
+
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+
+    {
+      title: "Data Analysis skills",
+      skills: [
+        {
+          name: "Power BI",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/c/cf/New_Power_BI_Logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "NumPy",
+          image:
+            "https://logo.svgcdn.com/l/numpy.svg",
+        },
+        {
+          name: "Pandas",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSu9xFbA6COOd9Wq-koFEoAFD7wpFgbvdz6Q&s",
+        },
+        {
+          name: "Matpilotlib",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/8/84/Matplotlib_icon.svg",
+        },
+        
+      ],
+    },
+
+    {
+      title: "Others",
+      skills: [
+        
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy6QCAHH1A1RdxwO9J-xDua2JYisHXcatrMg&s",
+      role: "Trainee",
+      company: "MSC Mediterranean Shipping Company",
+      date: "January 2025 - Current ",
+      desc: "During my internship, I am gaining valuable hands-on experience in web development, focusing on HTML, CSS, JavaScript, SQL, and PHP. I worked on real projects, learned to create responsive layouts, implement dynamic functionality, and manage databases. With guidance, I overcame challenges, honed my skills, and emerged ready to contribute effectively to future projects",
+    },
+   
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzhp7ckLOQRlUvwLoBOa8GQH3XN_yOZ1Q1-w&s",
+      role: "Web developer Iternship",
+      company: "QualityWebs Indore",
+      date: "April 2024 - June 2024 ",
+      desc: "During my internship, I am gaining valuable hands-on experience in web development, focusing on HTML, CSS, JavaScript, SQL, and PHP. I worked on real projects, learned to create responsive layouts, implement dynamic functionality, and manage databases. With guidance, I overcame challenges, honed my skills, and emerged ready to contribute effectively to future projects",
+    },
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqhNdol97ydxGx1FYhTqRHnSTCL90fFSZoIwElwo5ZPyTsFUkrNBWsTOKsTkgplj3ekT8&usqp=CAU",
+      role: "Full Stack Web developer Internship Program",
+      company: "Edureka! ",
+      date: "May 2022 - March 2023",
+      desc: "Engaged in an intensive internship program at Edureka focusing on full-stack web development. Acquired comprehensive hands-on experience with a spectrum of web technologies and frameworks. Developed proficiency in HTML, CSS, JavaScript, Bootstrap, React JS, Redux JS, MongoDB, Express JS, and Node JS through rigorous practical training. Executed 7 projects during the internship period, showcasing diverse skills and competencies: Netflix Homepage Clone, Simple Arithmetic Calculator, Weather App, Temperature Converter, E-commerce Web Application, Simple two player dice game, fashion store home page. Developed a strong foundation in full-stack web development during immersive internship at Edureka; honed practical skills, executed complex projects, and mastered cutting-edge technologies used in the industry.",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8sgv2Zl8w8cVCaveXknjMTchocEdlw_u5A&s",
+      school: "St. Aloysius Institute of Technology Jabalpur",
+      date: "2019 - 2023",
+      grade: "7.56 CGPA",
+      desc: "I have successfully completed a Bachelor's degree in Computer Science and Engineering from St. Aloysius Institute of Technology, Jabalpur, with an overall CGPA of 7.56. Throughout my studies, I gained a strong foundation by taking key courses such as Object-Oriented Programming, Database Management Systems, and Computer Networks, among others. These courses have equipped me with the technical skills and knowledge essential for solving complex computing problems and developing innovative software solutions.",
+      degree: "Bachelor of Technology - Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9kfgENGMYh7v6L5FUSKgfntO4mCOt5bqC3Q&s",
+      school: "O.F.K.E.E.H.S. School, Jabalpur",
+      date: " 2018 - 2019",
+      grade: "72%",
+      desc: "I completed my Class 12 education with a focus on Physics, Chemistry, and Mathematics (PCM) at O.F.K.E.E.H.S. School in Jabalpur. This solid academic foundation helped build my analytical and problem-solving skills, which have since played a crucial role in shaping my technical expertise.",
+      degree: "12th (PCM), State Board",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9kfgENGMYh7v6L5FUSKgfntO4mCOt5bqC3Q&s",
+      school: "O.F.K.E.E.H.S. School, Jabalpur",
+      date: "2016 - 2017",
+      grade: "85%",
+      desc: "I successfully completed my class 10 education at OFKEEHS School in Jabalpur, where I built a strong foundation in various academic subjects. The school provided a supportive learning environment, helping me develop essential skills and knowledge that have contributed to my educational and personal growth in subsequent years.",
+      degree: "10th - State board",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Fation Store E-commerce",
+      description:
+        "The MERN E-Commerce Web Application is a modern and feature-rich platform designed to facilitate online buying and selling of products. Built using the MERN (MongoDB, Express.js, React.js, Node.js) stack, this application leverages the power of JavaScript across the entire development stack, offering a seamless and efficient development experience. Integrated secure payment gateway using stripe pay platform, enhancing online transaction capabilities. Implemented JWT-based user authentication for secure access control.",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXdXZIUHozC2mg05dvp0x7DBP4DnCUNR0DnQusqp=CAU",
+      tags: ["HTML", "CSS", "BOOTSTRAP", "JavaScript", "React js","Redux","MongoDB","Express","Node js"],
+      category: "web app",
+      github: "https://github.com/Ramgopalpatel/MERN-fashion-store",
+    },
+    {
+      id: 1,
+      title: "Number Guesser",
+      description:
+        " The Number Guesser Game is a simple web-based game designed to entertain users by challenging them to guess a randomly generated number within a specified range (0-100). This project is implemented using HTML, CSS, and JavaScript, making it accessible to users across various platforms and devices without the need for additional software installation. The Number Guesser Game project offers an entertaining and interactive experience for users while showcasing fundamental concepts of web development using HTML, CSS, and JavaScript. ",
+      image:
+        "https://i.ytimg.com/vi/iIwALoXqJ5c/maesdefault.jpg",
+      tags: [ "HTML", "CSS", "JavaScript"],
+      category: "web app",
+      github: "https://github.com/Ramgopalpatel/number-guessing",
+      
+    },
+    {
+      id: 2,
+      title: "Weather App",
+      
+      description:
+        "Stay ahead of the forecast with our intuitive weather app. Get real-time updates on current conditions, hourly forecasts, and extended outlooks, all presented in a user-friendly interface. Whether you're planning your day or preparing for the week ahead, our app provides accurate and reliable weather information to help you make informed decisions. Download now and never get caught off guard by the weather again!",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXdXZIUHozC2mg05dvp0x7DBP4DnCUNR0DnQ&usqp=CAU",
+      tags: ["HTML", "CSS", "JavaSrcipt"],
+      category: "web app",
+      github: "https://github.com/Ramgopalpatel/Weather-website",
+    },
+    {
+      id: 3,
+      title: "Mobile Sales Analysis",
+      
+      description:
+        "Project Description: This interactive dashboard provides a comprehensive analysis of Motorola's mobile sales performance. It enables stakeholders to make data-driven decisions by visualizing key metrics and trends. The dashboard includes the following insights- Total Sales Overview: Displays aggregate sales revenue (₹160M), quantity sold (4K units), and the number of transactions (775).Average Price: Highlights the average selling price of mobile models (₹40K).Geographical Insights: A visual map showing sales distribution across major cities in India, such as Delhi, Mumbai, and Hyderabad.Sales Trends by Month: Line chart depicting the quantity sold yearly, with peaks in April and June.Customer Ratings: A horizontal bar chart summarizing customer satisfaction ratings on a scale of 1 to 5.Sales by Mobile Model: A comparison of revenue generated by models like iPhone SE, OnePlus Nord, and Galaxy Note 20, with Galaxy Note 20 leading at ₹56M.Payment Method Preferences: Pie chart showing the distribution of payment methods, including UPI, debit card, cash, and credit card.Sales by Day: Analysis of daily sales patterns, highlighting Saturday as the highest-performing day.Key Insights:Galaxy Note 20 is the top-selling model.Sales peak during weekends, particularly on Saturdays.Credit cards and debit cards are the most preferred payment methods.April and June see higher sales volumes compared to other months.Tools & Technologies Used:Microsoft Power BI for data visualization.Integrated geospatial analysis using Bing Maps.Interactive filters for dynamic exploration of sales data (by city, mobile model, ratings, and more).",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXdXZIUHozC2mg05dvp0x7DBP4DnCUNR0DnQusqp=CAU",
+      tags: ["Power BI"],
+      category: "others",
+      github: "https://github.com/Ramgopalpatel/Mobile-sale-data-analysis",
+    },
+    {
+      id: 4,
+      title: "Hotel Booking Cancellations Analysis",
+      
+      description:
+        "Project Title: Hotel Booking Cancellations AnalysisProject Description: This Excel-based dashboard provides a detailed analysis of hotel booking data, focusing on successful bookings and cancellations. It offers insights into guest behavior, room preferences, and booking patterns, enabling hotel management to make strategic decisions. The dashboard includes the following key components:Total Overview:Total Successful Bookings: 21,996 Total Cancellations: 8,142 Guest Type Analysis: A bar chart visualizing total bookings and cancellations by guest type (Couples, Family, Single). Couples represent the largest guest segment, with 15,998 total bookings but also 6,662 cancellations. Room Desirability Analysis: Total bookings for desired and undesired room types. Desired room types dominate with 18,160 bookings and 8,010 cancellations. Month-wise Analysis: Monthly trends showing total bookings and cancellations. September and October show the highest bookings, while cancellations peak in July and September. Hotel Type Analysis: Distribution of bookings and cancellations between City Hotels and Resort Hotels. City Hotels account for a higher number of bookings (13,682), but Resort Hotels show relatively more cancellations (2,138). Slicers for Dynamic Filtering: Year-based slicer (2015, 2016, 2017) for analyzing trends across specific timeframes. Key Insights: Couples are the most frequent guests but also have the highest cancellation rates. Desired room types experience significantly more bookings and cancellations compared to undesired ones. Peak cancellation periods align with high booking months, indicating potential overbooking or guest dissatisfaction. City Hotels outperform Resort Hotels in bookings, though cancellations in Resort Hotels are relatively higher. Tools & Technologies Used: Microsoft Excel for data aggregation and visualization. Charts and slicers for interactive and dynamic analysis.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXdXZIUHozC2mg05dvp0x7DBP4DnCUNR0DnQusqp=CAU",
+      tags: ["Excel"],
+      category: "others",
+      github: "https://github.com/Ramgopalpatel/Hotel-booking-cancellations",
+    },
+  ];
+  
+  
